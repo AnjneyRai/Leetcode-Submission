c5438058c64abb835043759bef4d7e04eaf7230e
Anjney Rai <anjneyrai81@gmail.com>
2026-09-02 15:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (91.75%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int scoreOfString(string s) {
     int sum=0;int diff;
-    int l=s.size();
-    for(int i=0;i<l-1;i++)
+    
+    for(int i=0;i<s.size()-1;i++)
     {   diff = abs(s[i]-s[i+1]);
         sum = sum + diff;
     }
