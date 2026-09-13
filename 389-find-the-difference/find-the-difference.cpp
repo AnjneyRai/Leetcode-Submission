d0c4bcf001dc4a0d90086aad3b3543c41eba36a2
Anjney Rai <anjneyrai81@gmail.com>
2026-09-13 20:05:00 +0530

    Time: 4 ms (4.14%) | Memory: 9.2 MB (50.83%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i] != t[i])
+        {   char ch = t[i];
+            return ch;          
+        }
+    }
+    int n = t.size();
+    return t[n-1];
+
+    }
+};
